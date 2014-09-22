3a13741960dbf27a2bce147461533829210fb688
jzjzme <jackiez3895@gmail.com>
2014-09-22 01:21:56 -0700

    Initial push of Storage
    header file for storage

---
+++ BoxIn/BoxIn/Storage.h
@@ -0,0 +1,41 @@
+/**
+BoxIn To-Do Task Manager
+In this file, the Storage component is added on
+**/
+
+#ifndef BoxInStorage
+#define BoxInStorage
+
+#include <string>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+namespace BoxIn {
+
+	class Inputlist;
+	class Storage{
+	public:
+		Storage();
+		vector<InputList> getInputList;
+		bool commit();
+
+	protected:
+		void setBoxes; //put the stuff in specific boxes/directories
+		void boxFiles;
+		string getsetBoxes;
+		string getboxfiles;
+		void clearFileStream();
+		void setFileStream();
+		void setUpInputList();
+
+	private:
+		string dataBoxesPath;
+		string dataFilePath;
+		vector<fstream> dataFileStream;
+		vector<InputList> _inputList;
+	}
+}
+
+#endif
