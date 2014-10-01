87c040c67e2346f0aba25c3736fcf1ea54f28f34
jzjzme <jackiez3895@gmail.com>
2014-10-01 09:24:33 -0700

    Events revision 2
    Edits and added things into events

--- BoxIn/BoxIn/Event.cpp
+++ BoxIn/BoxIn/Event.cpp
@@ -17,3 +17,49 @@ Event::~Event(void){
 std::string Event::getName(){
 	return name;
 }
+
+
+std::string Event::getDescription() {
+    return _description;
+}
+
+std::string Event::getCategory() {
+    return _category;
+}
+
+
+std::int Event::getPriority() {
+     return _priority;
+}
+
+bool Event::setName(string name) {
+     _name = name;
+     return true;
+}
+
+bool Task::setPriority(int value) {
+     _priority = value;
+     return true;
+}
+
+
+bool Task::setCategory(string cat) {
+     _category = cat;
+     return true;
+}
+
+
+bool Task::setDescription(string content) {
+      _description = content;
+      return true;
+}
+
+/**
+* Method: setFinish-->sets the task as done/undone. returns the final status of the _isDone variable
+*/
+
+bool Task::setFinish(bool result) {
+     _isDone = result;
+     return _isDone;
+}
+
