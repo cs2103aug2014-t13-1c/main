5fc847fb39867692655eff4aaca44472c39ad3ee
jzjzme <jackiez3895@gmail.com>
2014-10-15 19:58:00 -0700

    Edited the Logging
    exception included already

--- BoxIn/BoxIn/Storage.cpp
+++ BoxIn/BoxIn/Storage.cpp
@@ -57,17 +57,15 @@ namespace BoxIn {
 		if (dataFileStream->fail()) {
 			throw runtime_error("[Storage] Unable to create the data file. ");
 		}
-	}
 
-}
-int main(int, char*[])
-{
-    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-    BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
     BOOST_LOG_TRIVIAL(info) << "An informational severity message";
     BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
     BOOST_LOG_TRIVIAL(error) << "An error severity message";
     BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+	}
 
-    return 0;
 }
+
+
