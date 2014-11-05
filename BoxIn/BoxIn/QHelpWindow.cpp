69ca19979fc124323fe44ac83635f41409302cfd
emman <eygohlolz@gmail.com>
2014-11-05 16:02:34 +0800

    Neatened help text

--- BoxIn/BoxIn/QHelpWindow.cpp
+++ BoxIn/BoxIn/QHelpWindow.cpp
@@ -28,18 +28,18 @@ void QHelpWindow::setupSelector(){
 
 void QHelpWindow::changed(QString option){
     if(option == HelpOptions::GENERAL){
-        display->setText("BoxIn commands have the first word as the type of action desired.\nDifferent options are prefixed by the tags place, sdate (start date), stime (start time), edate (end date) and etime (endtime)\nDates are represented in the format DDMMYY or YYYY-Mon-DD where Mon is the 3-letter representation of the month\nIn the case where a word the user intends to type is a keyword, it can be escaped by prefixing the word with a . - for example, .place");
+        display->setText("BoxIn commands have the first word as the type of action desired.\n\nDifferent options are prefixed by the tags place, sdate (start date), stime (start time), edate (end date) and etime (endtime)\n\nDates are represented in the format DDMMYY or YYYY-Mon-DD where Mon is the 3-letter representation of the month\nIn the case where a word the user intends to type is a keyword, it can be escaped by prefixing the word with a . - for example, .place");
     }else if(option == HelpOptions::ADD){
-        display->setText("The add command follows the following syntax:\nadd <name> <optional fields>\nA full example is given below\nadd Change the world edate 311299 place Mars\nThis represents a plan to change the world before 31st December 2099 on Mars");
+        display->setText("The add command follows the following syntax:\n\nadd <name> <optional fields>\n\nA full example is given below\nadd Change the world edate 311299 place Mars\n\nThis represents a plan to change the world before 31st December 2099 on Mars");
     }else if(option == HelpOptions::DELETE){
-        display->setText("The delete command follows the following syntax:\ndelete <indexnumber>\nFor example:\ndelete 1\nThis deletes the item which matches the index 1. The index is found to the left of the event name.");
+        display->setText("The delete command follows the following syntax:\n\ndelete <indexnumber>\nFor example:\ndelete 1\n\nThis deletes the item which matches the index 1. The index is found to the left of the event name.");
     }else if(option == HelpOptions::EDIT){
-        display->setText("The easiest way to edit an event is to double click it and use the GUI interface\nAlternatively, the format for doing so is edit <indexnumber> field <fieldtag> <newvalue>\nA full example is given below\nedit 1 field name Change the Universe");
+        display->setText("The easiest way to edit an event is to double click it and use the GUI interface\nAlternatively, the format for doing so is \nedit <indexnumber> field <fieldtag> <newvalue>\n\nA full example is given below\nedit 1 field name Change the Universe");
     }else if(option == HelpOptions::MARK){
-        display->setText("The mark command changes the done/undone status of an event. The syntax for doing so is mark <indexnumber>\nFor example,\nmark 1\nwill mark the Change the Universe event as done");
+        display->setText("The mark command changes the done/undone status of an event. The syntax for doing so is \nmark <indexnumber>\n\nFor example,\nmark 1\nwill mark the Change the Universe event as done");
     }else if(option == HelpOptions::VIEW){
-        display->setText("The view command shows only specific events. By default, all events are displayed. The different options to view are\nall, past, upcoming, done and pending\nThe format for doing so is view <option>\nFor example,\nview done");
+        display->setText("The view command shows only specific events. By default, all events are displayed. The different options to view are\n\nall, past, upcoming, done and pending\n\nThe format for doing so is \nview <option>\nFor example,\nview done");
     }else if(option == HelpOptions::UNDO){
-        display->setText("The undo function allows a user to undo as many actions as they want up to the point the app was started. The syntax for doing so is simply to type undo");
+        display->setText("The undo function allows a user to undo as many actions as they want up to the point the app was started. The syntax for doing so is simply to type \nundo");
     }
 }
